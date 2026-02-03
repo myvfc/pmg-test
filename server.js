db40301c7c38fbdd58ac591d227e3a1edfad85ba
myvfc <154605226+myvfc@users.noreply.github.com>
2026-02-03 11:00:40 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
 import axios from "axios";
+
+// Fix __dirname in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = 3000;
